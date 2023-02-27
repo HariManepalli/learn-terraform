b1c3343072aec031674243ab924af6d28a7ba31c
Hariprasad <hari20185@gmail.com>
2023-02-27 11:39:16 +0530

    new changes-27 Feb

--- 05-s3-state/main.tf
+++ 05-s3-state/main.tf
@@ -1,16 +1,16 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-b71"
+    bucket = "terraform-b71m"
     key    = "05-s3-state/terraform.tfstate"
     region = "us-east-1"
   }
 }
 
 resource "aws_instance" "ec2" {
-  ami                    = "ami-0a017d8ceb274537d"
-  instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+  ami                     = "ami-0a017d8ceb274537d"
+  instance_type           = "t3.micro"
+  vpc_security_group_ids  = ["sg-0c59953308f220e16"]
   tags = {
-    Name = "demo"
+    Name = "test"
   }
 }
