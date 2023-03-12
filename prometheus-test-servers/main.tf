777c41f7fe9ffc810046e174d4f79abcd7d96fdf
Hariprasad <hari20185@gmail.com>
2023-03-12 11:15:29 +0530

    prometheus learning

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -18,6 +18,8 @@ resource "aws_spot_instance_request" "node2" {
   ami                    = "ami-065978add90d876d2"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0c59953308f220e16"]
+  wait_for_fulfillment    = true
+
   tags = {
     Name = "prom-test-node"
   }
